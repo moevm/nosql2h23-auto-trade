cbad144fd4eb0134e067014a1393f9d9057a2c19
VadimArkhipov <arkhipov_vadim@inbox.ru>
2023-09-25 23:02:34 +0300

    added write to database function

--- hello_world/app.js
+++ hello_world/app.js
@@ -4,7 +4,21 @@ const url = "mongodb://127.0.0.1:27017/";
 const name_db = 'hellodatabase';
 const name_collection = 'hellocollection';
 
+async function writeToDatabase() {
+    const mongoClient = new MongoClient(url);
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db(name_db);
+        const collection = db.collection(name_collection);
 
+        const data = await collection.insertOne({ message: 'HelloWorld' });
+        console.log('Data was successfully written to the database:', data.acknowledged, data.insertedId);
+    } catch (error) {
+        console.error('An error has occurred:', error);
+    } finally {
+        await mongoClient.close();
+    }
+}
 
 writeToDatabase().then(() => {
     readFromDatabase().then(() => {
