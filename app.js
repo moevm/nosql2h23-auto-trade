197b9e3dc14b654f2f3678b63b17d206ef1728be
proitshnik <mister.kalmak@yandex.ru>
2023-12-04 03:26:08 +0300

    Add export and import database functions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var routes = require('./routes');
 var cookieParser = require('cookie-parser');
 var session = require('express-session')
 var path = require('path');
+const fs = require('fs');
 
 server.use(cookieParser());
 server.use(express.json());
@@ -17,3 +18,60 @@ server.use('/',routes);
 server.listen(3000);
 module.exports = server;
 
+var url = "mongodb://localhost:27017/";
+const docker_status = false;
+if (docker_status) url = "mongodb://mongo:27017/";
+const MongoClient = require("mongodb").MongoClient;
+const name_db = 'autotrade';
+const name_collection = 'users';
+async function exportDatabase() {
+    const mongoClient = new MongoClient(url);
+    try {
+        console.log("Imhereexport");
+        await mongoClient.connect();
+        const db = mongoClient.db(name_db);
+        const collection = db.collection(name_collection);
+
+        data = await collection.find({}).toArray();
+        const jsonData = JSON.stringify(data);
+        // console.log(jsonData);
+        fs.writeFileSync('backup.json', jsonData);
+        console.log('Данные успешно записаны в backup.json');
+        // fs.writeFile('backup.json', jsonData, (err) => {
+        //     if (err) {
+        //         console.error('Ошибка записи в файл:', err);
+        //         return;
+        //     }
+        //
+        //     console.log('Данные успешно записаны в backup.json');
+        // });
+    } catch (error) {
+        console.error('An error has occurred:', error);
+    } finally {
+        await mongoClient.close();
+    }
+}
+// exportDatabase().then(r => {});
+
+async function importDatabase() {
+    const mongoClient = new MongoClient(url);
+    try {
+        console.log("Imhereimport");
+        await mongoClient.connect();
+        const db = mongoClient.db(name_db);
+        const collection = db.collection(name_collection);
+
+        await collection.deleteMany({});
+
+        const fileData = fs.readFileSync('backup.json');
+        console.log('Данные успешно прочитаны из backup.json');
+        const jsonData = JSON.parse(fileData);
+        const result = await collection.insertMany(jsonData);
+        console.log(`Импорт прошел успешно. ${result.insertedCount} элементов было добавлено в коллекцию users.`);
+    } catch (error) {
+        console.error('An error has occurred:', error);
+    } finally {
+        await mongoClient.close();
+    }
+}
+// importDatabase().then(r => {});
