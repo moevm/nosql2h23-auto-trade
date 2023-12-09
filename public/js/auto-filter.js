5198fbfa86bca992ab681bc0be1ab9715739f8e5
PetrBodunov <pbod@list.ru>
2023-12-09 12:14:45 +0300

    finish auto filter pug

--- public/js/auto-filter.js
+++ public/js/auto-filter.js
@@ -1,23 +1,42 @@
+let db_dataset = {
+    'Марка': [],
+    'Модель': [],
+    'Год': [],
+    'Цвет': [],
+    'Кузов': [],
+    'Пробег': [],
+    'Двигатель': [],
+    'Коробка': [],
+    'Привод': [],
+    'Руль': []
+}
+
 function filter(input) {
-    console.log(input.innerHTML)
-
     let par = input.closest(".dropdown-el").getElementsByTagName('span')[0]
-    let name = par.innerHTML
-
-    console.log(input)
-    console.log(input.innerHTML)
 
     if (input.innerHTML === "Очистить"){
         par.innerHTML = par.title
+        db_dataset[par.title] = []
     }
     else{
-        par.innerHTML = input.innerHTML
+        db_dataset[par.title].push(input.innerHTML)
+        if (par.innerHTML === par.title)
+            par.innerHTML = input.innerHTML
+        else {
+            par.innerHTML += ' ' + input.innerHTML
+        }
+
     }
 }
 
 function sendForm(input){
-    let par = input.closest(".menus-filter").getElementsByClassName('name-filter')
-    for (let i = 0; i < par.length; i++)
-        console.log(par[i].innerHTML)
-    console.log(par)
+    let filter = input.closest(".menus-filter")
+    let data = filter.getElementsByClassName('name-filter')
+    let year = document.getElementById('year')
+    let mileage = document.getElementById('mileage')
+
+    db_dataset['Год'] = year.value
+    db_dataset['Пробег'] = mileage.value
+
+    console.log(db_dataset)
 }
