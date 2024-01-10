3f4d10aaa0839ad22247bfdc2b086a6cabd8fe95
proitshnik <mister.kalmak@yandex.ru>
2024-01-10 13:03:54 +0300

    Ready install script with support dev mode

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-docker compose build --no-cache
-chmod +x init_photos.sh
-./init_photos.sh
+if [ "$1" = "dev" ]
+then 
+    docker compose -f docker-compose-dev.yml build --no-cache || docker-compose -f docker-compose-dev.yml build --no-cache
+    chmod +x init_photos.sh
+    ./init_photos.sh
+else 
+    docker compose build --no-cache || docker-compose build --no-cache
+    chmod +x init_photos.sh
+    ./init_photos.sh
+fi
