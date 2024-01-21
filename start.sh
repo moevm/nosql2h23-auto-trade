922dd777c996a3399f78a637ff0f10c4d0f0773c
Daniil Kalmak <mister.kalmak@yandex.ru>
2024-01-21 23:08:54 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 if [ "$1" = "dev" ]
 then 
     docker compose -f docker-compose-dev.yml up || docker-compose -f docker-compose-dev.yml up
+elif [ "$1" = "git" ]
+then 
+    docker compose up -d || docker-compose up -d
 else 
     docker compose up || docker-compose up
 fi
